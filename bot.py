5d62187956fc0f1de73c865983472568ea98e96e
Yonca <73234933+yoncasen@users.noreply.github.com>
2025-10-10 22:24:18 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+from bot_token import TOKEN
+from password_generator import generate_password
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$parola'):
+        await message.channel.send(generate_password(10))
+    else:
+        await message.channel.send(message.content)
+
+client.run(TOKEN)
+
