51b2d9f8f450df7fa2998926dfcb9b0e9dd9609d
Yonca <73234933+yoncasen@users.noreply.github.com>
2025-10-10 22:24:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,25 @@
 import discord
-from bot_token import TOKEN
-from password_generator import generate_password
 
-# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
 intents = discord.Intents.default()
-# Mesajları okuma ayrıcalığını etkinleştirelim
 intents.message_content = True
-# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} olarak giriş yaptık.')
+    print(f'{client.user} olarak giriş yaptık')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('$merhaba'):
-        await message.channel.send("Selam!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    elif message.content.startswith('$parola'):
-        await message.channel.send(generate_password(10))
-    else:
-        await message.channel.send(message.content)
-
-client.run(TOKEN)
-
+    if message.content.startswith('$hello'):
+        await message.channel.send(f'Merhaba {client.user}! ben bir botum!')
+    elif  message.content.startswith('$heh'):
+        if len(message.content) > 4:
+            count_heh = int(message.content[4:])
+        else:
+            count_heh = 5
+        await message.channel.send("he" * count_heh)
+        
+client.run("GİZLİ TOKEN BURAYA")
